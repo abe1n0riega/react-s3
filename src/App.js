ed39704a732e7655832c249568ed88a6329f2f0a
Abel Noriega <abe1n0riega@outlook.com>
2021-09-04 12:18:56 -0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div id="cuadro">
+      <h1>Mi primer Hola Mundo</h1>
+      <h2>Nunca es tarde para continuar!</h2>
+    </div>
+  );
+}
